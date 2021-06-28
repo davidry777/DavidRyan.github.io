03095ed6949cce9f3feed72b0b70fa691ac74685
davidry777 <dryan0319@gmail.com>
2021-06-28 09:19:54 -0700

    Updated navigation bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,11 +5,15 @@ function Navbar() {
     const [click, setClick] = useState(false);
 
     const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
     return (
         <>
             <nav className='navbar'>
                 <div className='navbar-container'>
-                    <Link to='/' className='navbar-logo'>
+                    <Link 
+                        to='/' 
+                        className='navbar-logo'
+                    >
                         DAVID <i className='fab fa-typo3' />
                     </Link>
                     <div className='menu-icon' onClick={handleClick}>
@@ -17,7 +21,42 @@ function Navbar() {
                     </div>
                 </div>
                 <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                    
+                    <li className='nav-item'>
+                        <Link 
+                            to='/' 
+                            className='nav-links' 
+                            onClick={closeMobileMenu}
+                        >
+                            Home
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link 
+                            to='/about' 
+                            className='nav-links' 
+                            onClick={closeMobileMenu}
+                        >
+                            About
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link 
+                            to='/projects' 
+                            className='nav-links' 
+                            onClick={closeMobileMenu}
+                        >
+                            Projects
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link 
+                            to='/contact' 
+                            className='nav-links' 
+                            onClick={closeMobileMenu}
+                        >
+                            Contact
+                        </Link>
+                    </li>
                 </ul>
             </nav>
         </>

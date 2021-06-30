35391c5463a6de59553e0e86132aae8f349dee68
davidry777 <dryan0319@gmail.com>
2021-06-30 11:11:11 -0700

    fixed image issue in Card.js

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import CardItem from './CardItem';
 import './Cards.css';
+import img from '../images/img-9.jpg'
 
 function Cards() {
     return (
@@ -9,7 +10,12 @@ function Cards() {
             <div className="cards__container">
                 <div className="cards__wrapper">
                     <ul className="cards__items">
-                        <CardItem />
+                        <CardItem 
+                            src={img}
+                            text="Explore the hidden waterfall deep inside the Amazon Jungle"
+                            label='Adventure'
+                            path='/services'
+                        />
                     </ul>
                 </div>
             </div>

2cd0339805626f29c7289ec393f6fe58965fdb2b
davidry777 <dryan0319@gmail.com>
2021-06-30 08:32:51 -0700

    Updating Cards

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function CardItem() {
     return (
@@ -11,13 +11,13 @@ function CardItem() {
                     </figure>
                     <div className="cards__item__info">
                         <h5 className="cards__item__text">
-                            
+
                         </h5>
                     </div>
                 </Link>
             </li>
         </div>
-    )
+    );
 }
 
 export default CardItem

7e0e28204880c42b69ab5c821fcee9b4ec4e40da
davidry777 <dryan0319@gmail.com>
2021-07-25 10:04:28 -0700

    Changed color for footer. Removed sub items

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,60 +6,6 @@ import './Footer.css'
 function Footer() {
     return (
         <div className='footer-container'>
-            <section className='footer-subscription'>
-                <p className='footer-subscription-heading'>
-                    Check out my page daily for any updates.
-                </p>
-                <p className='footer-subscription-text'>
-                    For contact, go to my contact page.
-                </p>
-                <div className='input-areas'>
-                    <form>
-                        <input 
-                            type="email" 
-                            name="email" 
-                            placeholders="Your Email" 
-                            className="footer-input"
-                        />
-                        <Button buttonStyle='btn--outline'>Subscribe</Button>
-                    </form>
-                </div>
-            </section>
-            <div className="footer-links">
-                <div className="footer-link-wrapper">
-                    <div className='footer-link-items'>
-                        <h2>About us</h2>
-                        <Link to='/view-resume'>View Resume</Link>
-                        <Link to='/'>Testimonials</Link>
-                        <Link to='/'>Careers</Link>
-                        <Link to='/'>Investors</Link>
-                        <Link to='/'>Terms of Service</Link>
-                    </div>
-                    <div class='footer-link-items'>
-                        <h2>Contact Us</h2>
-                        <Link to='/'>Contact</Link>
-                        <Link to='/'>Support</Link>
-                        <Link to='/'>Destinations</Link>
-                        <Link to='/'>Sponsorships</Link>
-                    </div>
-                </div>
-                <div className='footer-link-wrapper'>
-                    <div class='footer-link-items'>
-                        <h2>Videos</h2>
-                        <Link to='/'>Submit Video</Link>
-                        <Link to='/'>Ambassadors</Link>
-                        <Link to='/'>Agency</Link>
-                        <Link to='/'>Influencer</Link>
-                    </div>
-                    <div class='footer-link-items'>
-                        <h2>Social Media</h2>
-                        <Link to='/'>Instagram</Link>
-                        <Link to='/'>Facebook</Link>
-                        <Link to='/'>Youtube</Link>
-                        <Link to='/'>Twitter</Link>
-                    </div>
-                </div>
-            </div>
             <section class='social-media'>
                 <div class='social-media-wrap'>
                     <div class='footer-logo'>
